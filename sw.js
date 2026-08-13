5434a0df4583de31d621fc052a5dabdd2ccbb73a
tahai-hsieh <tahai20090126@gmail.com>
2026-08-13 12:08:18 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'tahai-clock-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './icon.png'
+];
+
+// 1. 安裝 Service Worker 並快取檔案
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+// 2. 啟用並清理舊快取
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// 3. 離線攔截請求：優先從快取讀取
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        })
+    );
+});
